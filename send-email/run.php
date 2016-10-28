238759c9f6cd0b6bf08e61898df1ede305380dfb
Ori Pekelman <ori@platform.sh>
2016-10-28 13:17:27 +0200

    reverting to old and ugly, dont refactor twenty minutes before session

--- send-email/run.php
+++ send-email/run.php
@@ -1,22 +1,35 @@
 <?php
+ 
+$apiKey = $_SERVER['SEND_GRID_API_KEY'];
 $platform_deployer_app_url =$_SERVER['DEPLOY_OR_NOT_URL'];
 $api = 'https://api.sendgrid.com/v3/mail/send';
-
+$request = null;
+$url = null;
+$project = null;
+$payload = null;
+$environment = null;
+$machine_name = null;
 $res = new stdClass();
 $to = new stdClass();
 $from = new stdClass();
 $cont_element = new stdClass();
  
 function autolink($str) {
-  $str = ' ' . $str; 
-  $str = preg_replace('`([^"=\'>])((http|https)://[^\s<]+[^\s<\.)])`i', '$1<a  href="$2">$2</a>',$str);
-  $str = substr($str, 1);
-  return $str;
+$str = ' ' . $str;
+$str = preg_replace(
+     '`([^"=\'>])((http|https)://[^\s<]+[^\s<\.)])`i',
+     '$1<a  href="$2">$2</a>',
+     $str
+);
+$str = substr($str, 1);
+ 
+ return $str;
 }
-
 if (!getenv('req')) {
   throw new \Exception("Invalid request");
 }
+ 
+$request = json_decode(file_get_contents(getenv('req')));
  
 if (!property_exists($request, 'project')) {
   throw new \Exception("Invalid project");
@@ -25,18 +38,23 @@ if (!property_exists($request, 'project')) {
 if ($request->type!="environment.branch"){
   throw new \Exception("Only Supporting Branching for the moment");
 }
-
-$request = json_decode(file_get_contents(getenv('req')));
+ 
 $project = $request->project;
 $environment = $request->parameters->environment;
+$parent = $request->parameters->environment;
 $log = autolink($request->log);
+ 
+$deploy_or_not_url  = $platform_deployer_app_url ."?project=".$project . "&environment=".$environment;
+$deploy_or_not_link = '<a style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;" href="'. $deploy_or_not_url.'">'.$deploy_or_not_url."</a>";
 
-$content = str_replace("{{deploy_or_not_url}}", $platform_deployer_app_url ."?project=".$project . "&environment=".$environment ,file_get_contents("template.html"));
-$content = str_replace("{{environment}}", $request->parameters->environment,$content);
-$content = str_replace("{{parent}}", $request->parameters->environment,$content);
-$content = str_replace("{{log}}", $log, $content);
+$content = "Environment ".$environment." was just created and deployed from parent ". $parent. ".<br><br>\n\n";
+$content .= "<pre>".$log."</pre>";
+$content .= "You can click on the link above to review the changes.<br>";
+$content .= "And if you really like them,  use DeployOrNot to deploy (or not). by visiting:<br>\n\n";
+$content .= $deploy_or_not_link;
+$template = file_get_contents("template.html");
+$content = str_replace("{{content}}", $content ,$template);
 
-echo $content;
 $to->name="DeployOrNot";
 $to->email="ori+test@platform.sh";
 $from->name="DeployOrNot";
